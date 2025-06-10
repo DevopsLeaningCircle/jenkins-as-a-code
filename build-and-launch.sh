ef3cd153f8911dcac613645ef975f5b2a5bb3045
Naren Chejara <nchejara@opentext.com>
2025-06-10 11:43:52 +0800

    add a configuration file

--- build-and-launch.sh
+++ build-and-launch.sh
@@ -5,12 +5,27 @@ set -e # exit immediately if any command fails
 BASE_IMAGE=jenkins/jenkins:latest
 IMAGE_NAME=jenkins-server
 TAG=latest
+CONTAINER_NAME=Jenkins-server
+
+# Set real permisison for plugins.txt, casc.yaml, and init.groovy.d\*
+sudo chmod +r plugins.txt casc.yaml
+sudo chmod +r init.groovy.d
 
 # Build Jenkins server docker image
 docker build -t $IMAGE_NAME:$TAG --build-arg BASE_IMAGE=$BASE_IMAGE .
 
+# Create a directory, option -p ensure no error if the directory already exists
+mkdir -p /jenkins 
+sudo chmod +rw /jenkins
+
+# Stop and remove container if it exists
+if [ "$(docker ps -a -q -f name=^/${CONTAINER_NAME}$)" ]; then
+    echo "Stopping and removing existing container: $CONTAINER_NAME"
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
 # Launch a jenkins server
-mkdir /jenkins
 docker run -itd -p 8080:8080 --name Jenkins-server -v /jenkins:/var/jenkins_home $IMAGE_NAME:$TAG
 
 # Running with ldap-env file
--- dockerfile
+++ dockerfile
@@ -7,15 +7,23 @@ RUN apt-get update && apt-get install -y curl git
 
 USER jenkins
 
+# Diable Setup Wizard dialog
+ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+# Set CASC_JENKINS_CONFIG env variable 
+ENV CASC_JENKINS_CONFIG=/var/jenkins_home/casc.yaml
+
 # Pre-install plugins
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
 
-# Add Groovy init scripts
+
+# Copy init Groovy scripts to ref, these scripts run one by one when a container launch first time
 COPY --chown=jenkins:jenkins init.groovy.d/ /usr/share/jenkins/ref/init.groovy.d/
+# Copy JCasC configuration to /var/jenkins_home
+COPY  --chown=jenkins:jenkins casc.yaml /var/jenkins_home/casc.yaml
 
 # Pre-create Jobs, config, etc. (optiona)
-# COPY jobs /usr/share/jenkins/ref/jobs
+# COPY -chown=jenkins:jenkins  jobs /usr/share/jenkins/ref/jobs
 
 # Expose default Jenkins port
 EXPOSE 8080
